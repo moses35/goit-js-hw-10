4a830d4ca217935891bd2bc3bfe5eecca4128ca9
moses35 <energetic143@gmail.com>
2023-04-15 20:29:10 +0300

    Update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
 function fetchCountries(name) {
-  let url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages `;
+  const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages `;
   return fetch(url).then(response => response.json());
 }
 export { fetchCountries };
